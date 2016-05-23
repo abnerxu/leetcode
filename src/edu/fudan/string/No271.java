1db7e94e7f0088b6886bc581235087cfb23dec16
abnerxu <abner_xu@foxmail.com>
2016-05-23 16:44:53 +0800

    add

--- src/edu/fudan/string/No271.java
+++ src/edu/fudan/string/No271.java
@@ -1,48 +1,85 @@
 package edu.fudan.string;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Encode and Decode Strings 
  * Design an algorithm to encode a list of strings to a string. The encoded string is then sent over the network and is decoded back to the original list of strings.
-
-Machine 1 (sender) has the function:
-
-string encode(vector<string> strs) {
-  // ... your code
-  return encoded_string;
-}
-Machine 2 (receiver) has the function:
-
-vector<string> decode(string s) {
-  //... your code
-  return strs;
-}
- 
-
-So Machine 1 does:
-
-string encoded_string = encode(strs);
- 
-
-and Machine 2 does:
-
-vector<string> strs2 = decode(encoded_string);
- 
-
-strs2 in Machine 2 should be the same as strs in Machine 1.
-
-Implement the encode and decode methods.
-
-Note:
-
-The string may contain any possible characters out of 256 valid ascii characters. Your algorithm should be generalized enough to work on any possible characters.
-Do not use class member/global/static variables to store states. Your encode and decode algorithms should be stateless.
-Do not rely on any library method such as eval or serialize methods. You should implement your own encode/decode algorithm.
+ * 
+ * Machine 1 (sender) has the function:
+ * string encode(vector<string> strs) {
+ *   // ... your code
+ *    return encoded_string;
+ * }
+ * 
+ * Machine 2 (receiver) has the function:
+ * vector<string> decode(string s) {
+ *   //... your code
+ *    return strs;
+ * }
+ *    
+ * So Machine 1 does:
+ * string encoded_string = encode(strs);
+ * 
+ * and Machine 2 does:
+ * vector<string> strs2 = decode(encoded_string);
+ * strs2 in Machine 2 should be the same as strs in Machine 1.
+ * Implement the encode and decode methods.
+ * Note:
+ * The string may contain any possible characters out of 256 valid ascii characters. 
+ * Your algorithm should be generalized enough to work on any possible characters.
+ * Do not use class member/global/static variables to store states. 
+ * Your encode and decode algorithms should be stateless.
+ * Do not rely on any library method such as eval or serialize methods. 
+ * You should implement your own encode/decode algorithm.
  * 
  * http://www.cnblogs.com/easonliu/p/4784912.html
+ * 
+ * {“abc”，“123”，“d"} -> 3#abc3#1231#d  ：res += to_string(str.length()) + "#" + str;
  * 
  * @author abner
  *
  */
 public class No271 {
+	
+	//编码方式：长度#字符串
+	 public static String encode(List<String> strs) {
+		 StringBuilder stringBuilder=new StringBuilder();
+		for (String str : strs)
+			stringBuilder.append(str.length() + "#").append(str);
+		 return stringBuilder.toString();
+	 }
+	 
+	 public static List<String> decode(String s) {
+		 List<String>result=new ArrayList<>();
+		 int start=0;
+		 while(start<s.length()){
+			 int index=s.indexOf('#', start);
+			 int size=Integer.parseInt(s.substring(start, index));
+			 result.add(s.substring(index+1, index+size+1));
+			 start=index+size+1;
+		 }		 
+		 return result;
+	 }
+	 
+	
+	public static void main(String args[]) throws Exception{
+		BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+		
+		String str=null;
+		List<String> input=new ArrayList<>();
+		while((str=bufferedReader.readLine())!=null){
+			input.add(str);
+		}
+		bufferedReader.close();
+		
+		List<String> result=decode(encode(input));
+		for (int i = 0; i < result.size(); i++) {
+			System.out.println(input.get(i).equals(result.get(i)));
+		}
+	}
 
 }
---
+++ src/edu/fudan/string/No340.java
@@ -0,0 +1,116 @@
+package edu.fudan.string;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * Given a string, find the length of the longest substring T that contains at most 2 distinct characters. * 
+ * For example, Given s = ¡°eceba¡±, 
+ * T is "ece" which its length is 3. 
+ * 
+ * @author abner 
+ * reference:http://blog.csdn.net/whuwangyi/article/details/42451289
+ *
+ */
+public class No340 {
+	
+	
+	/**
+	 * map<Character,Integer>  key£ºcharacter value:count
+	 * @param s
+	 * @param k
+	 * @return
+	 * 
+	 * O(N)
+	 */
+	public static int lengthOfLongestSubstringKDistinct(String s, int k) { 
+		int start=0;
+		int maxLen=0;
+		
+		Map<Character,Integer> slidingWindow=new HashMap<>();
+		
+		for (int i = 0; i < s.length(); i++) {
+			char c=s.charAt(i);
+			// when c is already existed,just increment the count
+			if(slidingWindow.containsKey(c)){
+				slidingWindow.put(c, slidingWindow.get(c)+1);
+			}else{
+				slidingWindow.put(c, 1);
+				
+				//when the size of slidingWindow exceeds k,delete the c orderly one by one
+				while(slidingWindow.size()>k){
+					char tmp=s.charAt(start++);
+					int count=slidingWindow.get(tmp);
+					if(count<=1){
+						slidingWindow.remove(tmp);
+					}else{
+						slidingWindow.put(tmp, count-1);
+					}
+				}
+			}
+			maxLen=Math.max(maxLen, i-start+1);
+		}
+		return maxLen;
+	}
+	
+	/**
+	 * sliding window : map<Character,Integer>  key£ºcharacter value:position
+	 * delete the most left character which appeared last.
+	 * should find the new start position every time.
+	 * O(N*K)
+	 * @param s
+	 * @return
+	 */
+	public static int lengthOfLongestSubstringTwoDistinct(String s){
+		int startPos=0;
+		int maxLen=0;
+		
+		Map<Character,Integer> slidingWindow=new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			char tmp=s.charAt(i);
+			//when current slidingWindow at least has two different character and TMP is a new character
+			if(slidingWindow.size()>=2&&!slidingWindow.containsKey(tmp)){
+				//find the most left character in the slidingWindow
+				char mostLeftChar=findMostLeftPos(slidingWindow,s);				
+				//get the position of the most left character in the slidingWindow
+				int mostLeftPos=slidingWindow.get(mostLeftChar);
+				//remove the opposite character
+				slidingWindow.remove(mostLeftChar);				
+				//update startPos
+				startPos=mostLeftPos+1;
+			}
+			//when there is less than two different character or TMP is not a new character
+			//put a new character or update the last position of the existed character
+			slidingWindow.put(tmp, i);
+			//update the value of MAXLEN
+			maxLen=Math.max(maxLen, i-startPos+1);
+		}
+		
+		return maxLen;
+	}
+	
+	private static char findMostLeftPos(Map<Character,Integer> map,String s){
+		char result=' ';
+		int pos=s.length();
+		for (char c : map.keySet()) {
+			if(map.get(c)<pos){
+				result=c;
+				pos=map.get(c);
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String args[]) throws Exception{
+		BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
+		String str=null;		
+		while((str=bufferedReader.readLine())!=null){
+			System.out.println(lengthOfLongestSubstringKDistinct(str, 2));
+			//System.out.println(lengthOfLongestSubstringTwoDistinct(str));
+		}
+		bufferedReader.close();
+	}
+}
